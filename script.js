ea61c53f5d6a9809ba244b391082cd25a05615f3
Uday517 <132931649+Uday517@users.noreply.github.com>
2024-02-06 00:17:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const yesButton = document.getElementById("yes");
+const noButton = document.getElementById("no");
+
+yesButton.addEventListener("click", function() {
+    window.location.href = "yes.html";
+});
+
+noButton.addEventListener("click", function() {
+    alert("Nahh that ain't gonna work dummy 😂😂😂");
+});
